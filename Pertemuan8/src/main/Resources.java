932f2ea27d37bea3e59190f799e38d2a1f9ebe53
rivaldy913 <rivaldynovyandp@gmail.com>
2024-12-11 00:41:48 +0700

    Mengerjakan Pertemuan 8

---
+++ Pertemuan8/src/main/Resources.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.io.InputStream;
+
+public interface Resources {
+
+    static InputStream getResourceAsStream(String resource) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getResourceAsStream'");
+    }
+
+}
